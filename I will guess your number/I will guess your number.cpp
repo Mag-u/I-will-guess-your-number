1ec7f5dddd5ad177c883e8b3c150e4f28f5ac9be
Michał Lipiec <magunesen@gmail.com>
2021-11-13 14:31:38 +0100

    v1.01

--- I will guess your number/I will guess your number.cpp
+++ I will guess your number/I will guess your number.cpp
@@ -6,6 +6,8 @@ int main()
 	int number = 0;
 	int startingNumber = 0;
 	int endingNumber = 0;
+	int copy = 0;
+	string stopProgram = "";
 
 	cout << "Input the range from which you selected the number \n";
 	cout << "The first number: ";
@@ -14,8 +16,29 @@ int main()
 	cout << "The last number: ";
 	cin >> endingNumber;
 	cout << endl;
-
-
-
-
+	                                                                             
+	while (startingNumber == endingNumber)                                                    
+	{                                                                                           
+		cout << "The numbers are that same and becouse of it the game can't start" << endl;          
+		cout << "Do you want to input numbers again? Y/N";                                       
+		cin >> stopProgram;                                                                   
+		if (stopProgram != "Y")                                                               
+		{                                                                                      
+			exit(0);                                                                            
+		}
+		else {
+			cout << "The first number: ";
+			cin >> startingNumber;
+			cout << endl;
+			cout << "The last number: ";
+			cin >> endingNumber;
+			cout << endl;
+		}
+	}
+	if (startingNumber > endingNumber)
+	{
+		copy = endingNumber;
+		endingNumber = startingNumber;
+		startingNumber = copy;
+	}
 }
