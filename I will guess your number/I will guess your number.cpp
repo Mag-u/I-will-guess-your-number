480335d5261ee29ec6ba2276af4fead862147148
Michał Lipiec <magunesen@gmail.com>
2021-11-13 14:23:47 +0100

    Dodaj pliki projektów.

---
+++ I will guess your number/I will guess your number.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	int startingNumber = 0;
+	int endingNumber = 0;
+
+	cout << "Input the range from which you selected the number \n";
+	cout << "The first number: ";
+	cin >> startingNumber;
+	cout << endl;
+	cout << "The last number: ";
+	cin >> endingNumber;
+	cout << endl;
+
+
+
+
+}
